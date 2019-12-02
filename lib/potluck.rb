f02e66730c43e3d91ac3805f0e82f6be045b982b
jerry <jerry@jerrys-MacBook-Pro.local>
2019-12-02 11:30:52 -0700

    Add potluck pt 3 (100%)

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,3 +1,5 @@
+require_relative 'dish.rb'
+
 class Potluck
 	attr_reader :date, :dishes
 
@@ -10,12 +12,41 @@ class Potluck
 		@dishes << dish
 	end
 	
-	def get_all_from_category(desired_category)
+	def get_all_from_category(category)
+		dishes_in_category = []
 		dishes_in_category = @dishes.select do |dish|
-					dish.category == desired_category
+					dish.category == category
 					end
 		return dishes_in_category
 	end
+	
+	def menu
+		menu = {}
+		appetizers = []
+		get_all_from_category(:appetizer).each do |dish|
+			appetizers << dish.name
+			menu[:appetizers] = appetizers.sort
+			end	
+		entres = []
+		get_all_from_category(:entre).each do |dish|
+			entres << dish.name
+			menu[:entres] = entres.sort
+			end
+		desserts = []
+		get_all_from_category(:dessert).each do |dish|
+			desserts << dish.name
+			menu[:desserts] = desserts.sort
+			end
+		return menu		
+	end
+
+	def ratio(category)
+		if @dishes == []
+			return "no dishes"
+		else
+			get_all_from_category(category).count.to_f / @dishes.count * 100
+		end
+	end	
 	
 end
 
